2e1598a8f5dc2de9c382f5ba6ce72cb75ea4d0d6
Uzhvenko Andrey <uzhvenko@mail.ru>
2023-02-10 22:44:38 +0300

    сохраняем

---
+++ task5/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача №7. Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 	456 -> 6
+// 	782 -> 2
+// 	918 -> 8
+/// ПЕРВЫЙ ВАРИАНТ
+
+// Console.Write("Введи трёхзначное число: ");
+// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// string stringNumber = Convert.ToString(threeDigitNumber);
+// Console.WriteLine("третья цифра этого числа -> "+stringNumber[2]);
+
+/// ВТОРОЙ ВАРИАНТ
+
+Console.Write("Введите трехзначное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int LastNumber = 0;
+if(N > 99 && N < 1000)
+{
+    LastNumber = N%10;
+    Console.Write(LastNumber);
+}
+else
+{
+    Console.Write("Некорректный ввод");
+}
